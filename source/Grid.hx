0a07f7f3d5ff4256fa34d00c91e5c7898d41a918
Ryan Kubik <rmkubik@me.com>
2021-06-21 17:26:35 -0400

    Create grid abstraction

--- source/Grid.hx
+++ source/Grid.hx
@@ -1,7 +1,55 @@
-class Grid<T> {
-    var cells:Array<Array<T>>;
+import Matricies.Location;
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.graphics.FlxGraphic;
+import flixel.math.FlxPoint;
+import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
+import flixel.tile.FlxTilemap;
 
-  	public function new(cells) {
-      this.cells = cells;
-    }
+function convertPointToLocation(tileSize:Int, point:FlxPoint) {
+	return new Location(Math.floor((point.y - Config.instance.topMargin) / tileSize), Math.floor(point.x / tileSize));
+}
+
+class Grid {
+  var tilesheet:FlxGraphic;
+  public var layers:Array<FlxTilemap> = [];
+  var position:FlxPoint;
+  var state:FlxState;
+  var onClick:(Location)->Void = (location) -> {};
+
+  public function new(position, tilesheet, state, onClick) {
+    this.tilesheet = tilesheet;
+    this.position = position;
+    this.state = state;
+    this.onClick = onClick;
+  }
+
+  public function createLayer(tileData) {
+    var tiles = new FlxTilemap();
+    tiles.loadMapFrom2DArray(
+      tileData,
+      tilesheet,
+      Config.instance.tileSize,
+      Config.instance.tileSize,
+      FlxTilemapAutoTiling.OFF,
+      1,
+      1
+    );
+    tiles.setPosition(this.position.x, this.position.y);
+    
+    state.add(tiles);
+
+    this.layers.push(tiles);
+  }
+
+  public function update() {
+    if (FlxG.mouse.justPressed) {
+			var position = FlxG.mouse.getPosition();
+			var location = convertPointToLocation(Config.instance.tileSize, position);
+			
+			trace('mouse pressed at: ' + position + ' -> ' + location.trace());
+
+      onClick(location);
+		}
+  }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,8 +10,6 @@ import flixel.math.FlxPoint;
 import flixel.math.FlxRandom;
 import flixel.system.scaleModes.RatioScaleMode;
 import flixel.text.FlxText;
-import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
-import flixel.tile.FlxTilemap;
 import flixel.util.FlxColor;
 
 function pickRandom<T>(rand:FlxRandom, items:Array<T>) {
@@ -20,10 +18,6 @@ function pickRandom<T>(rand:FlxRandom, items:Array<T>) {
 
 function pickRandomWeighted<T>(rand:FlxRandom, items:Array<T>, weights:Array<Float>) {
 	return items[rand.weightedPick(weights)];
-}
-
-function convertPointToLocation(tileSize:Int, point:FlxPoint) {
-	return new Location(Math.floor((point.y - Config.instance.topMargin) / tileSize), Math.floor(point.x / tileSize));
 }
 
 var TileTypes = {
@@ -49,23 +43,26 @@ var TileTypeInfo:Map<Int, Any> = [
 
 class PlayState extends FlxState
 {
-	var tiles:FlxTilemap;
-	var upperLayer:FlxTilemap;
 	var rand:FlxRandom;
-	var tower:Tower;
 	var gameState:String;
+	
+	var grid:Grid;
+	var BACKGROUND_LAYER = 0;
+	var TILES_LAYER = 1;
+	var UPPER_LAYER = 2;
+
 	var food:Int;
 	var gold:Int;
 	var stone:Int;
 	var wood:Int;
-	var foodText:FlxText;
-	var goldText:FlxText;
-	var stoneText:FlxText;
-	var woodText:FlxText;
+
+	var resourceTextRow:TextRow;
+	
 	var selected:Location = null;
+	
 	var infoTitleText:FlxText;
 	var infoBodyTest:FlxText;
-	var resourceTextRow:TextRow;
+	var buildGrid:Grid;
 
 	override public function create()
 	{
@@ -76,10 +73,9 @@ class PlayState extends FlxState
 		var tilesBitmapData = openfl.Assets.getBitmapData(AssetPaths.sprites__png);
 		var tilesheet:FlxGraphic = FlxGraphic.fromBitmapData(tilesBitmapData);
 
-		var ITEM_COUNT = 20;
 		var width = Config.instance.tilesWide;
 		var height = Config.instance.tilesHigh;
-		var bugs:Array<Int> = [13, 14, 17, 18, 27, 29];
+
 		// placingStart, playing, placing
 		gameState = 'placingStart';
 		food = 0;
@@ -89,8 +85,25 @@ class PlayState extends FlxState
 
 		bgColor = new FlxColor(0xFF663931);
 
-		var background = new FlxTilemap();
-		background.loadMapFrom2DArray(
+		grid = new Grid(
+			new FlxPoint(0, Config.instance.topMargin),
+			tilesheet,
+			this,
+			(location) -> {
+				if (gameState == 'placingStart') {
+					location.setTile(grid.layers[TILES_LAYER], TileTypes.VILLAGE);
+					gameState = 'playing';
+				} else if (gameState == 'playing') {
+					if (selected != null) {
+						selected.setTile(grid.layers[UPPER_LAYER], TileTypes.EMPTY);
+					}
+					
+					selected = location;
+				}
+			}
+		);
+
+		grid.createLayer(
 			constructMatrix((row, col) -> {
 				var isEvenRow = row % 2 == 0;
 				var isEvenCol = col % 2 == 0;
@@ -105,48 +118,17 @@ class PlayState extends FlxState
 				} else {
 					return 22;
 				}
-			}, width, height),
-			tilesheet,
-			Config.instance.tileSize,
-			Config.instance.tileSize,
-			FlxTilemapAutoTiling.OFF,
-			1,
-			1
+			}, width, height)	
 		);
-		background.setPosition(0, Config.instance.topMargin);
-		add(background);
 
 		var tileData = constructMatrix(
 			(row, col) -> pickRandomWeighted(rand, [TileTypes.EMPTY, TileTypes.TREE, TileTypes.BUSH_BERRY, TileTypes.STONE, TileTypes.GOLD], [75, 17, 6, 2, 1]),
 			width,
 			height
 		);
-
-		tiles = new FlxTilemap();
-		tiles.loadMapFrom2DArray(
-			tileData,
-			tilesheet,
-			Config.instance.tileSize,
-			Config.instance.tileSize,
-			FlxTilemapAutoTiling.OFF,
-			1,
-			1
-		);
-		tiles.setPosition(0, Config.instance.topMargin);
-		add(tiles);
-
-		upperLayer = new FlxTilemap();
-		upperLayer.loadMapFrom2DArray(
-			constructMatrix((row, col) -> 0, width, height),
-			tilesheet,
-			Config.instance.tileSize,
-			Config.instance.tileSize,
-			FlxTilemapAutoTiling.OFF,
-			1,
-			1
-		);
-		upperLayer.setPosition(0, Config.instance.topMargin);
-		add(upperLayer);
+		grid.createLayer(tileData);
+		
+		grid.createLayer(constructMatrix((row, col) -> 0, width, height));
 
 		// top bar
 		var textWidth = Config.instance.tileSize * 2;
@@ -160,6 +142,7 @@ class PlayState extends FlxState
 			0,
 			Config.instance.pixelsHigh - Config.instance.bottomMargin
 		);
+		
 		 
 		
 		// info bar on far right
@@ -186,28 +169,12 @@ class PlayState extends FlxState
 
 		updateTextBar();
 
-		if (FlxG.mouse.justPressed) {
-			var position = FlxG.mouse.getPosition();
-			var location = convertPointToLocation(Config.instance.tileSize, position);
-			
-			trace('mouse pressed at: ' + position + ' -> ' + location.trace());
-
-			if (gameState == 'placingStart') {
-				location.setTile(tiles, TileTypes.VILLAGE);
-				gameState = 'playing';
-			} else if (gameState == 'playing') {
-				if (selected != null) {
-					selected.setTile(upperLayer, TileTypes.EMPTY);
-				}
-				
-				selected = location;
-			}
-		}
+		grid.update();
 
 		if (selected != null) {
-			selected.setTile(upperLayer, TileTypes.SELECTOR);
+			selected.setTile(grid.layers[UPPER_LAYER], TileTypes.SELECTOR);
 
-			var selectedTile = selected.getTile(tiles);
+			var selectedTile = selected.getTile(grid.layers[TILES_LAYER]);
 			infoTitleText.text = TileTypeInfo[selectedTile];
 		}
 	}
